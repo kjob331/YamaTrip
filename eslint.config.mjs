4793e626be13db89866e2f347a8566678219d3ba
Nakamura <k.nakamurajob1997@gmail.com>
2026-07-05 17:03:34 +0900

    test github actions lint

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+export default defineConfig([
+  ...nextVitals,
+
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      quotes: ["warn", "double"],
+      "prefer-const": "warn",
+    },
+  },
+
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
